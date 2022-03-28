c54d6f931ebdf2b3cc5c2978f6486b033b353de5
aolmkt <elton@alternativacuritiba.com.br>
2022-03-28 08:35:19 -0300

    19:07

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ $( document ).ready(function() {
  
   if(!localStorage.getItem('_bannershow_')){
       localStorage.setItem('_bannershow_', true);
-      setTimeout(function(){ $('#lp-addtocart').fadeIn(); }, 1140000);
+      setTimeout(function(){ $('#lp-addtocart').fadeIn(); }, 1144200);
 
     }else {
        $('#lp-addtocart').fadeIn();
@@ -54,7 +54,7 @@ $( document ).ready(function() {
   }, 1111);
 
 	
-	setTimeout(function(){ $("#float-btn").show(); }, 1140000);		
+	setTimeout(function(){ $("#float-btn").show(); }, 1144200);		
 });
 
 function addLoadEvent(func) {
